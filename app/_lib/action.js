53a3519c3178f33787c5bcd67cf4ce024b6a6e4c
amsein <shabanishabaniamirhossein98@gmail.com>
2025-07-17 18:48:36 +0330

    The Reservations section finished!

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,9 +1,11 @@
 "use server";
 
 import { auth, signIn, signOut } from "./auth";
+import { getBooking, getBookings } from "./data-service";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
 
+// ACTION FOR UPDATE GUEST PROFILE :
 export async function updateGuest(formData) {
   // console.log(formData);
   const session = await auth();
@@ -27,10 +29,33 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+//ACTION FOR DELETE RESERVATION'S
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in ⛔");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("you are not allowed to delete this booking ❌");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted 🤔");
+
+  revalidatePath("/account/reservations");
+}
+
+// ACTION FOR SIGN IN
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
 
+// ACTION FOR SIGN OUT
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
